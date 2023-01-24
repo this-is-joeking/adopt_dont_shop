9ceb9cf236fb05d0c66b4001a74fcf9f6c71f316
Joe King <j.king92@gmail.com>
2023-01-24 10:08:00 -0700

    Remove comment

--- spec/features/admins/applications/show_spec.rb
+++ spec/features/admins/applications/show_spec.rb
@@ -217,11 +217,3 @@ RSpec.describe 'admin application show' do
     end
   end
 end
-
-# As a visitor
-# When a pet has an "Approved" application on them
-# And when the pet has a "Pending" application on them
-# And I visit the admin application show page for the pending application
-# Then next to the pet I do not see a button to approve them
-# And instead I see a message that this pet has been approved for adoption
-# And I do see a button to reject them
