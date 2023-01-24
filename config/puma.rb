6aa9e86a1dfda6459e41e631bdce3082b187406d
Joe King <j.king92@gmail.com>
2023-01-24 10:38:52 -0700

    Add file utils to puma

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,10 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+
+require "fileutils"
+FileUtils.mkdir_p("tmp/pids")
+
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
